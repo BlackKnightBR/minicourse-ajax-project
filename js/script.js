654a9799437560def28502a708949f808b9876bd
Rorigo Montebello Saboya Brito <blackknight@blackknightstudio.com.br>
2017-04-26 16:35:10 -0300

    Versão com as 3 APIs funcionais.

--- js/script.js
+++ js/script.js
@@ -11,9 +11,51 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    //Google streetview.
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
 
-    // YOUR CODE GOES HERE!
+    $greeting.text('So you want to live at ' + address + '?');
+
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='
+    + address + ' ';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    //NYT Ajax request.
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=f8252dd6ae4243ca9208c51f10c06e79';
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+
+    //Wikipedia Ajax request.
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+
+    $.ajax(wikiUrl,{
+      dataType: "jsonp",
+      success: function(response){
+        var articleList = response[1];
+
+        for(var i = 0; i < articleList.length; i++){
+          articleStr = articleList[i];
+          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+          $wikiElem.append('<li><a href="' + url + '">"' + articleStr + '</a></li>');
+        };
+      }
+    });
 
     return false;
 };
